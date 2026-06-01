079c0449eca70a36df06569a6f6a345b413008ab
matbed6332 <mateusz.com2@gmail.com>
2026-06-01 10:21:23 +0200

    s

--- Funckje.py
+++ Funckje.py
@@ -1,3 +1,8 @@
+from sklearn.metrics import root_mean_squared_error,mean_absolute_error
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 def percent_missing(df):
     percent_nan = 100 * df.isnull().sum() / len(df)
     percent_nan = percent_nan[percent_nan > 0].sort_values()
@@ -23,3 +28,20 @@ def regresive_model_split(df,X):
     df['split'] = split
 
     return df
+
+def run_model(model,X_train,y_train,X_test,y_test):
+    '''
+    Do importu
+    from sklearn.metrics import root_mean_squared_error,mean_absolute_error
+    '''
+    model.fit(X_train,y_train)
+
+    y_test_pred = model.predict(X_test)
+    y_train_pred = model.predict(X_train)
+
+    results = {"RMSE_train": root_mean_squared_error(y_train, y_train_pred),
+        "MAE_train": mean_absolute_error(y_train, y_train_pred),
+        "RMSE_test": root_mean_squared_error(y_test, y_test_pred),
+        "MAE_test": mean_absolute_error(y_test, y_test_pred)}
+
+    return model, results, y_test_pred, y_train_pred
--- MLR.py
+++ MLR.py
@@ -3,9 +3,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from Funckje import percent_missing
+from Funckje import percent_missing, regresive_model_split, run_model
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import root_mean_squared_error,mean_absolute_error
 
-st.title("Welcome to MLR modeling")
+
+st.title("Welcome to Machine learning")
 
 uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
 
@@ -73,4 +81,154 @@ if uploaded_file is not None:
         split_df = regresive_model_split(sorted_df.copy(), test_every)
 
         st.write(split_df["split"].value_counts())
-        st.dataframe(split_df.head())
+        st.dataframe(split_df)
+
+    fig, ax = plt.subplots(figsize=(10,4),dpi = 200)
+
+    ax.scatter(
+        split_df.index,
+        split_df[y_col],
+        c=split_df["split"].map({"Train": "blue", "Test": "red"})
+    )
+
+    ax.set_xlabel("Sorted compounds")
+    ax.set_ylabel(y_col)
+
+    st.pyplot(fig)
+
+    if "split" in split_df.columns:
+
+        train_df = split_df[split_df["split"] == "Train"]
+        test_df = split_df[split_df["split"] == "Test"]
+
+        X_train = train_df.drop(columns=[y_col, "split"])
+        y_train = train_df[y_col]
+
+        X_test = test_df.drop(columns=[y_col, "split"])
+        y_test = test_df[y_col]
+
+        st.subheader("Model Selection")
+
+        model_name = st.selectbox(
+            "Choose model",
+            ["MLR", "PCR", "PLS", "SVR", "RFR"]
+        )
+
+        if model_name == "MLR":
+            model = LinearRegression()
+
+        elif model_name == "PCR":
+
+            n_comp = st.slider(
+                "Number of PCA components",
+                1,
+                min(X_train.shape[1], 20),
+                5
+            )
+
+            model = Pipeline([
+                ("pca", PCA(n_components=n_comp)),
+                ("regressor", LinearRegression())
+            ])
+
+        elif model_name == "PLS":
+
+            n_comp = st.slider(
+                "Number of PLS components",
+                1,
+                min(X_train.shape[1], 20),
+                5
+            )
+
+            model = PLSRegression(n_components=n_comp)
+
+        elif model_name == "SVR":
+
+            C = st.number_input("C", value=1.0)
+
+            model = SVR(
+                kernel="rbf",
+                C=C
+            )
+
+        elif model_name == "RFR":
+
+            n_estimators = st.slider(
+                "Number of trees",
+                50,
+                1000,
+                200
+            )
+
+            model = RandomForestRegressor(
+                n_estimators=n_estimators,
+                random_state=42
+            )
+
+        if st.button("Run model"):
+            model, results, y_test_pred, y_train_pred = run_model(
+                model,
+                X_train,
+                y_train,
+                X_test,
+                y_test)
+
+        col1, col2 = st.columns([1, 2])
+
+        with col1:
+
+            metrics_df = pd.DataFrame(
+                {
+                    "Metric": results.keys(),
+                    "Value": results.values()
+                }
+            )
+
+            st.subheader("Model Performance")
+            st.dataframe(
+                metrics_df,
+                hide_index=True,
+                use_container_width=True
+            )
+
+        with col2:
+
+            fig, ax = plt.subplots(figsize=(6, 6))
+
+            ax.scatter(
+                y_train, y_train_pred,
+                label="Train",
+                alpha=0.6
+            )
+
+            ax.scatter(
+                y_test, y_test_pred,
+                label="Test",
+                alpha=0.6
+            )
+
+            # wspólna oś odniesienia
+            all_vals = np.concatenate([
+                y_train.values,
+                y_test.values,
+                y_train_pred,
+                y_test_pred
+            ])
+
+            min_val = all_vals.min()
+            max_val = all_vals.max()
+
+            ax.plot(
+                [min_val, max_val],
+                [min_val, max_val],
+                '--',
+                color='black')
+
+            ax.set_xlabel("Observed")
+            ax.set_ylabel("Predicted")
+            ax.set_title("Observed vs Predicted")
+
+            ax.legend()
+            ax.grid(True)
+
+            st.pyplot(fig)
